af35b7fde79a639791c2bb8f58d24f2face2db3a
amarcomon <a.marcomon@gmail.com>
2021-04-03 23:21:10 +0100

    feat(app): component moduling and header structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, HeaderComponent, MenuComponent],
   imports: [CommonModule],
+  exports: [DashboardComponent, HeaderComponent, MenuComponent],
 })
 export class ComponentsModule {}
