cb4e4fbda0613a0d43caf4d8eef33ebd33630a61
amarcomon <a.marcomon@gmail.com>
2021-04-04 23:15:41 +0100

    feat(core): add angular material and create modal

--- src/app/shared/components.module.ts
+++ src/app/shared/components.module.ts
@@ -4,10 +4,30 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { WeekDayComponent } from './dashboard/components/week-day/week-day.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ModalComponent as ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [DashboardComponent, HeaderComponent, MenuComponent, WeekDayComponent],
-  imports: [CommonModule],
-  exports: [DashboardComponent, HeaderComponent, MenuComponent],
+  declarations: [
+    DashboardComponent,
+    HeaderComponent,
+    MenuComponent,
+    WeekDayComponent,
+    ModalComponent,
+  ],
+  imports: [CommonModule,    
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule],
+  exports: [
+    DashboardComponent,
+    HeaderComponent,
+    MenuComponent,
+    WeekDayComponent,
+    ModalComponent,
+  ],
+  entryComponents: [ModalComponent]
 })
 export class ComponentsModule {}
