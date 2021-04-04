cf68ac6294be1c852eb3f9de96885bcce3fc9697
amarcomon <a.marcomon@gmail.com>
2021-04-04 01:08:21 +0100

    feat(dashboard): add week data to dashboard

---
+++ src/app/components/dashboard/dashboard.constants.ts
@@ -0,0 +1,166 @@
+export const WEEK_DATA = [
+  {
+    day: 'LUNES 15',
+    events: [
+      {
+        meeting_type: '1',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '2',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '3',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+  {
+    day: 'MARTES 16',
+    events: [
+      {
+        meeting_type: '4',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '2',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+  {
+    day: 'MIÉRCOLES 17',
+    events: [
+      {
+        meeting_type: '2',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '4',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+  {
+    day: 'JUEVES 18',
+    events: [
+      {
+        meeting_type: '5',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '2',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+  {
+    day: 'VIERNES 19',
+    events: [
+      {
+        meeting_type: '4',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '2',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+  {
+    day: 'SÁBADO 20',
+    events: [],
+  },
+  {
+    day: 'DOMINGO 21',
+    events: [
+      {
+        meeting_type: '1',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+      {
+        meeting_type: '3',
+        title: 'Praesent bibendum',
+        subtitle: '00000000',
+        content: {
+          title: 'Lorem ipsum dolor sit amet, consectetur.',
+          description:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
+      },
+    ],
+  },
+];
