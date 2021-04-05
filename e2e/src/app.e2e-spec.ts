56f70d22e38334784065792ceacc53c4b185b7c4
amarcomon <a.marcomon@gmail.com>
2021-04-05 17:19:26 +0100

    feat(modal): add test e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,39 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display header', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('calendar-app app is running!');
+    expect(await page.getHeaderContainer()).toBeTruthy();
   });
 
+  it('should display aside menu', async () => {
+    await page.navigateTo();
+    expect(await page.getAsideMenu()).toBeTruthy();
+  });
+
+  it('should display dashboard title', async () => {
+    await page.navigateTo();
+    expect(await page.getDashboardTitle()).toEqual('Dashboard');
+  });
+
+  it('should display day container', async () => {
+    await page.navigateTo();
+    expect(await page.getDayEventContainer()).toBeTruthy();
+  });
+
+  it('should show modal when user clicks on day event', async () => {
+    await page.navigateTo();
+    const events = await page.getEvents();
+    await events[0].click();
+    expect(await page.getModalContainer()).toBeTruthy();
+  });
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,43 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getDashboardTitle(): Promise<string> {
+    return element(by.css('.dashboard-title')).getText();
+  }
+
+  async getHeaderContainer(): Promise<any> {
+    return element(by.css('.header-container'));
+  }
+
+  async getAsideMenu(): Promise<any> {
+    return element(by.css('.menu-container'));
+  }
+
+  async getDayEventContainer(): Promise<any> {
+    return element(by.css('.day-container'));
+  }
+
+  async getDateContainer(): Promise<any> {
+    return element(by.css('.date-container'));
+  }
+
+  async getEventsContainer(): Promise<any> {
+    return element(by.css('.events-container'));
+  }
+
+  getEvents(): ElementArrayFinder {
+    return element.all(by.css('.event'));
+  }
+
+  getModalContainer(): ElementFinder {
+    return element(by.css('.modal-container'));
+  }
+
+  getCloseIconModal(): ElementFinder {
+    return element(by.css('.close'));
   }
 }
