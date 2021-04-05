b8d408b48c130f035fe64ec4964bad4c1af7d104
amarcomon <a.marcomon@gmail.com>
2021-04-05 01:07:21 +0100

    feat(modal): set modal style

--- src/app/shared/dashboard/components/week-day/week-day.component.ts
+++ src/app/shared/dashboard/components/week-day/week-day.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from '../../../modal/modal.component';
 
 @Component({
   selector: 'app-week-day',
@@ -9,7 +11,19 @@ export class WeekDayComponent implements OnInit {
   @Input() date: string;
   @Input() events: string;
 
-  constructor() {}
+  constructor(private matDialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  openModal(event) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = false;
+    dialogConfig.id = 'modal-component';
+    dialogConfig.height = 'auto';
+    dialogConfig.width = '350px';
+    dialogConfig.data = event;
+
+    this.matDialog.open(ModalComponent, dialogConfig);
+  }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private readonly BACKGROUND_COLOR = {
+    1 : 'lightpink',
+    2 : 'lightsteelblue',
+    3 : 'mediumseagreen',
+    4 : 'mediumturquoise',
+    5 : 'sandybrown'
   }
 
+  constructor(
+    private dialogRef: MatDialogRef<ModalComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit(): void {}
+  
+  closeModal() {
+    this.dialogRef.close();
+  }
+
+  setBackgroundColor() {
+    return this.BACKGROUND_COLOR[this.data.meeting_type]
+  }
 }
